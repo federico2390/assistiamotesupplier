754b54e501e9ae906e512f93f8f8823ed8a1bf54
federico2390 <cure-90@hotmail.it>
2022-12-11 15:34:48 +0100

    fixes

--- lib/globals/button.dart
+++ lib/globals/button.dart
@@ -24,6 +24,7 @@ class Button extends StatelessWidget {
         ? TextButton(
             style: TextButton.styleFrom(
               backgroundColor: color ?? AppColors.primaryColor,
+              padding: icon != null ? EdgeInsets.zero : null,
             ),
             onPressed: onPressed,
             child: icon ??
